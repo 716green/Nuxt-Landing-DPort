2ff7cf5dcd12b861ca5f7a058133ea3165e8affd
716green <bob@bbass.co>
2020-08-17 04:43:10 -0400

    created basic layout with vuetify

---
+++ components/VuetifyLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <img
+    class="DPLogo"
+    alt="Debt Portfol.io Logo"
+    src="/debt-portfolio.svg"
+  >
+</template>
+
+<style>
+.DPLogo {
+  height: 60px;
+  width: 350px;
+  transform: rotateX(90deg);
+  animation: turn 1.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
